f51a7a9aea8e739c02cd04c0da78762093718815
eorichards <eorichar@ucsd.edu>
2018-03-01 10:40:37 -0800

    thirdpartyAPI lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
-	// add your code here
+	// create center marker at UCSD
+	var ucsd_ltlng = {lat:32.879012, lng:-117.235834}
+
+	//create a map object and specify the DOM element for display
+	var map = new google.maps.Map(document.getElementById('map'),{
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: 'UCSD'
+	});
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -2,9 +2,12 @@ var data = {
   rsvp: ['ixd@ucsd.edu']
 };
 
-/*
- * GET tasks page.
- */
+exports.addRSVP = function(req, res){
+	var rsvpEmail = req.body.rsvpEmail;
+		console.log(rsvpEmail);
+		res.send(rsvpEmail);
+}
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
